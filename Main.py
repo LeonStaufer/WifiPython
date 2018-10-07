dc42e51167f017e9e80cbc5fc31f7164c7fdf4c6
Leon Staufer <leon.staufer@googlemail.com>
2018-10-07 20:32:31 +0200

    cleaned up code added three examples

--- Main.py
+++ Main.py
@@ -3,15 +3,14 @@ from pprint import pprint
 import matplotlib.pyplot as plt
 
 import Data
-from Point import Point
-from Trilaterate import trilaterate_mathematical, trilaterate_approx, trilaterate_opti
+from Trilaterate import trilaterate_opti
 from WiFi import WiFi
 
-wifi = WiFi("raw/mathDoor.json")
+wifi = WiFi("raw/businessDoor.json")
 
 scale = 61
 
-plt.figure(dpi=400)
+plt.figure(dpi=400, figsize=(7, 3))
 
 ax = plt.gca(aspect='equal')
 ax.cla()
@@ -21,54 +20,43 @@ ax.set_ylim((0, 1200))
 image = plt.imread("plan1.png")
 plt.imshow(image, zorder=0, extent=[0, 3028, 0, 1251])
 
-
-# plt.xlabel("X")
-# plt.ylabel("Y")
-# plt.suptitle("WiFi Access Points")
-
 points = []
 values = wifi.load(wifi.read(), False, "FIS")
 points2 = []
 
+"""
+    helper function for returning the predominant height
+    allows the trilateration to take place in only 2 dimensions
+"""
+
+
+def predominant_height(point_list):
+    lst = []
+    for val in point_list:
+        point = Data.addresses.get(val[0][:-3])
+        lst.append(point.z)
+
+    return max(set(lst), key=lst.count)
+
+
+predom_height = predominant_height(values)
+
 for val in values:
     point = Data.addresses.get(val[0][:-3])
-    if point.z is 4:
+    if point.z is predom_height:
         points.append(point)
         dict = [point.x, point.y, val[1].toDistance()]
         points2.append(dict)
         radius = val[1].toDistance() * scale
         ax.add_artist(plt.Circle((point.x*scale, point.y*scale), radius, color="#0F0F0F1F", alpha=0.5))
 
-#result = trilaterate_mathematical(points, values, scale)
-"""
-print(trilaterate_approx([
-    Point(21.8, 3.74, 4),
-    Point(31.2, 3.74, 6),
-    Point(31.2, 3.74, 4),
-    Point(21.15, 12, 6),
-    Point(7, 3.74, 6),
-    Point(21.8, 3.74, 6),
-    Point(47.3, 13.87, 6),
-    Point(40.64, 3.74, 6),
-    Point(47.3, 13.87, 4),
-    Point(40.64, 3.74, 4),
-], [
-    5.768968649587894,
-    19.67147263933248,
-    1.6941731714390469,
-    14.37303644749458,
-    29.072944794470832,
-    3.6399731395334642,
-    14.471166480939697,
-    2.677718046538936,
-    16.757730182816537,
-    4.569936016072616
-]))
-"""
-
 
 tripoint = trilaterate_opti(points2)
-
+pprint(tripoint)
+ax.add_artist(plt.Circle((42*scale, 7.5*scale), 20, color="blue"))
 ax.add_artist(plt.Circle((tripoint[0]*scale, tripoint[1]*scale), 20, color="red"))
 
+plt.axis("off")
+plt.tight_layout()
+plt.savefig("assets/business.png")
 plt.show()
--- Trilaterate.py
+++ Trilaterate.py
@@ -3,11 +3,9 @@ import math
 import numpy
 import scipy.optimize
 
-from Point import Point
-
-ITER = 4
-ALPHA = 2
-RATIO = 0.99
+"""
+    mathematical approach to trilateration
+"""
 
 
 def trilaterate_mathematical(points, values, scale):
@@ -27,7 +25,7 @@ def trilaterate_mathematical(points, values, scale):
         elif counter == 2:
             DistC = val[1].toDistance() * scale
 
-    # from wikipedia
+    # from Wikipedia derivation
     # transform to get circle 1 at origin
     # transform to get circle 2 on x axis
     ex = (P2 - P1) / (numpy.linalg.norm(P2 - P1))
@@ -37,8 +35,8 @@ def trilaterate_mathematical(points, values, scale):
     d = numpy.linalg.norm(P2 - P1)
     j = numpy.dot(ey, P3 - P1)
 
-    # from wikipedia
-    # plug and chug using above values
+    # from Wikipedia derivation
+    # insert all values and solve
     x = (pow(DistA, 2) - pow(DistB, 2) + pow(d, 2)) / (2 * d)
     y = ((pow(DistA, 2) - pow(DistC, 2) + pow(i, 2) + pow(j, 2)) / (2 * j)) - ((i / j) * x)
 
@@ -48,50 +46,9 @@ def trilaterate_mathematical(points, values, scale):
 
     z = numpy.sqrt(pow(DistA, 2) - pow(x, 2) - pow(y, 2))
 
-    # triPt is an array with ECEF x,y,z of trilateration point
-    triPt = P1 + x * ex + y * ey + z * ez
-    print(triPt.item(0))
-    print(triPt)
-    return triPt
-
-
-"""
-    Trilateration solution from StackOverflow, ported from C++
-"""
-
-
-def trilaterate_approx(points, dist):
-    res = Point()
-    alpha = ALPHA
-    for iter in range(ITER):
-        delta = Point()
-        for i in range(len(points)):
-            d = res.distanceFrom(points[i])
-            diff = (points[i] - res) * (alpha * (d - dist[i] / max(dist[i], d)))
-            delta = delta + diff
-        delta = delta * (1.0 / len(points))
-        alpha = alpha * RATIO
-        res = res + delta
-    return res
-
-
-"""
-    function to be optimized    
-"""
-
-
-def distance(point1, point2):
-    return math.sqrt(
-        (point1[0] - point2[0]) ** 2 +
-        (point1[1] - point2[1]) ** 2)
-
-
-def distance_sum(x, points):
-    distsum = 0
-    for c in points:
-        dist = distance(x, c) - x[2] * c[2]
-        distsum += pow(dist, 2)
-    return distsum
+    # triPt is an array with x,y,z of trilateration point
+    tri_point = P1 + x * ex + y * ey + z * ez
+    return tri_point
 
 
 """
@@ -100,8 +57,38 @@ def distance_sum(x, points):
 
 
 def trilaterate_opti(points):
-    limit = 10
-    count = 0
+    """
+        helper function
+    """
+    def _distance(point1, point2):
+        return math.sqrt(
+            (point1[0] - point2[0]) ** 2 +
+            (point1[1] - point2[1]) ** 2)
 
-    result = scipy.optimize.fmin(distance_sum, [0, 0, 0], args=(points,), xtol=0.0001, ftol=0.0001, disp=False)
+    """
+        helper function for average position
+    """
+    def _average(points):
+        x_sum, y_sum = 0, 0
+
+        for point in points:
+            x_sum += point[0]
+            y_sum += point[1]
+
+        x = int(x_sum/len(points))
+        y = int(y_sum/len(points))
+
+        return [x, y]
+
+    """
+        function to be optimized    
+    """
+    def _distance_sum(x, points):
+        distsum = 0
+        for c in points:
+            dist = _distance(x, c) - x[2] * c[2]
+            distsum += pow(dist, 2)
+        return distsum
+
+    result = scipy.optimize.fmin(_distance_sum, [_average(points)[0], _average(points)[1], 0], args=(points,), xtol=0.0001, ftol=0.0001, disp=False)
     return result
